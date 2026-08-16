62a90d1011c31f8df74bc4dc7768b9d65e8863d9
AgarOther <scraeyme@student.42angouleme.fr>
2026-08-16 15:27:29 +0200

    Added registration page

--- backend/database/routes.js
+++ backend/database/routes.js
@@ -4,21 +4,21 @@ import db from "./db.js";
 const router = express.Router();
 
 router.post('/addUser', async (req, res) => {
-	const { username } = req.body;
-	if (typeof username === 'undefined')
+	const { username, password, first_name, last_name, gender, email, city } = req.body;
+	if (!username || !password || !first_name || !last_name || !gender || !email || !city)
 		return res.status(400).send("Bad request");
-	let conn;
 	try
 	{
-		await db.run("INSERT INTO `test`(`username`) VALUES (?)", [username]);
+		await db.run("INSERT INTO `users` (username, password, first_name, last_name, gender, email, city) VALUES (?,?,?,?,?,?,?)",
+			[username, password, first_name, last_name, gender, email, city]
+		);
 
 		console.log(username + " logged.");
 	} catch (error)
 	{
 		console.error("grosse erreur!!! : " + error.message);
-	} finally
-	{
-		if (conn) await conn.release();
+		res.status(400).send();
+		return;
 	}
 	res.status(201).send();
 })
--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,18 +1,27 @@
 import { useState } from "react";
 
 function Register() {
-	const [name, setName] = useState("");
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+	const [first_name, setFirstName] = useState("");
+	const [last_name, setLastName] = useState("");
+	const [gender, setGender] = useState("");
+	const [email, setEmail] = useState("");
+	const [city, setCity] = useState("");
 
 	async function register() {
 		try {
 			const response = await fetch("http://localhost:3000/addUser", {
 				method: "POST",
 				headers: { "Content-Type": "application/json" },
-				body: JSON.stringify({username: name})
+				body: JSON.stringify({username, password, first_name, last_name, gender, email, city})
 			});
 
 			if (response.status == 400)
+			{
+				alert("Error!");
 				throw response.text();
+			}
 
 			alert("Done!");
 		} catch (error) {
@@ -22,9 +31,22 @@ function Register() {
 
 	return (
 		<>
-			<h1>Hello World!</h1>
-			<input type="text" placeholder="Your name..."/>
-			<button onClick={register} onChange={(event) => setName(event.target.value)}>CLIQUE MOIIIIIIIIIIIIII</button>
+			<div id="registration">
+				<h1>Register</h1>
+				<input type="text" placeholder="Username" autoComplete="username" value={username} onChange={(event) => setUsername(event.target.value)}/>
+				<input type="password" placeholder="Password" autoComplete="new-password" value={password} onChange={(event) => setPassword(event.target.value)}/>
+				<input type="text" placeholder="First name" autoComplete="given-name" value={first_name} onChange={(event) => setFirstName(event.target.value)}/>
+				<input type="text" placeholder="Last name" autoComplete="family-name" value={last_name} onChange={(event) => setLastName(event.target.value)}/>
+				<select value={gender} onChange={(event) => setGender(event.target.value)}>
+					<option value="">Gender...</option>
+					<option value="F">Female</option>
+					<option value="M">Male</option>
+					<option value="N">Non specified</option>
+				</select>
+				<input type="text" placeholder="Email" autoComplete="email" value={email} onChange={(event) => setEmail(event.target.value)}/>
+				<input type="text" placeholder="City" autoComplete="address-level2" value={city} onChange={(event) => setCity(event.target.value)}/>
+				<button onClick={register}>Register</button>
+			</div>
 		</>
 	);
 }
